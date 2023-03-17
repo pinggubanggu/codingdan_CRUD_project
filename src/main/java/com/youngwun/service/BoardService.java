b60c77f61035a2441b4621906b8f46c124167579
jyw <duddns6393@naver.com>
2023-03-17 10:18:21 +0900

    persistance working

---
+++ src/main/java/com/youngwun/service/BoardService.java
@@ -0,0 +1,19 @@
+package com.youngwun.service;
+
+import com.youngwun.domain.BoardVO;
+
+import java.util.List;
+
+public interface BoardService {
+
+    public void register(BoardVO board);
+
+    public BoardVO get(Long bno);
+
+    public boolean modify(BoardVO board);
+
+    public boolean remove(Long bno);
+
+    public List<BoardVO> getList();
+
+}
---
+++ src/main/java/com/youngwun/service/BoardServiceImpl.java
@@ -0,0 +1,4 @@
+package com.youngwun.service;
+
+public class BoardServiceImpl {
+}
