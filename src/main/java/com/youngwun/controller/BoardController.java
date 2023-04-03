1b0662ffcf2684c19a7241da35652e86362f9753
jyw <duddns6393@naver.com>
2023-04-03 09:32:20 +0900

    20230403 business-persentaion develop end

---
+++ src/main/java/com/youngwun/controller/BoardController.java
@@ -0,0 +1,68 @@
+package com.youngwun.controller;
+
+import com.youngwun.domain.BoardVO;
+import com.youngwun.service.BoardService;
+import lombok.extern.log4j.Log4j;
+import oracle.jdbc.proxy.annotation.Post;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@Log4j
+@RequestMapping("/board/*")
+public class BoardController {
+
+    @Autowired
+    private BoardService service;
+
+    @GetMapping("/list")
+    public void list(Model model) {
+        log.info("list");
+        model.addAttribute("list", service.getList());
+    }
+
+    @PostMapping("/register")
+    public String register(BoardVO board, RedirectAttributes rttr) {
+        log.info("register: " + board);
+
+        service.register(board);
+        rttr.addFlashAttribute("result", board.getBno());
+
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/get")
+    public void get(@RequestParam("bno") Long bno, Model model) {
+        log.info("/get");
+        model.addAttribute("board", service.get(bno));
+    }
+
+    @PostMapping("/modify")
+    public String modify(BoardVO board, RedirectAttributes rttr) {
+        log.info("modify:" + board);
+
+        if(service.modify(board)) {
+            rttr.addFlashAttribute("result", "success");
+        }
+
+        return "redirect:/board/list";
+    }
+
+    @PostMapping("/remove")
+    public String remove(@RequestParam("bno") Long bno, RedirectAttributes rttr) {
+        log.info("remove..." + bno);
+        if(service.remove(bno)) {
+            rttr.addFlashAttribute("result", "success");
+        }
+
+        return "redirect:/board/list";
+    }
+
+
+}
